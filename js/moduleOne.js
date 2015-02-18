4a727d2ebce120264c56e7199da74b2192ffc63c
Asa Toburen <atopettit@gmail.com>
2015-02-18 05:39:55 -0700

    fixed reset and form validation

--- js/moduleOne.js
+++ js/moduleOne.js
@@ -5,18 +5,6 @@ angular.module('firstModule', [
   ])
 
   .controller('FirstCtrl', ['$scope', function($scope) {
-    $scope.data = {
-      job_title : 'job title',
-      tedious_task : 'tedious task',
-      dirty_task : 'dirty task',
-      celebrity :  'celebrity',
-      useless_skill : 'useless skill',
-      obnoxious_celebrity : 'obnoxious celebrity',
-      number : 'huge number',
-      adjective :  'adjective',
-      Name : 'Name'
-    };
-
     
     $scope.gender = function() {
       if($scope.genderSingular === 'she') {
@@ -31,26 +19,15 @@ angular.module('firstModule', [
         $scope.genderPossessive = 'her';
       }
       };
-    $scope.submit = function() {
-        // if statements to check and identify individual form values
-
       
-    $scope.checkValid = function(reset) {
+    $scope.checkValid = function() {
       if($scope.myForm.$submitted && $scope.myForm.$valid && !($scope.myForm.$pristine)) {
         return true;
-      } else if (reset) {
-        return false; 
-      }
-    };
-      console.log($scope.myForm.$valid);
-      console.log($scope.myForm.$pristine);
+      } 
     };
 
    $scope.resetForm = function() {
-      //hide textarea and show form
-      // I might need to add ng-click or function on the reset button to cause ng-show/hide
-        //to evaluate to true;
-      $scope.data = {};
+        $scope.data = {};
+        $scope.myForm.$setPristine();
     };
-
 }]);
