15ab58a58d2f775ba2a7cfdcefbfc4a86a1385de
Asa Toburen <atopettit@gmail.com>
2015-02-17 19:18:44 -0700

    checked all form values, need to work on reset button and indiv form validation

--- js/moduleOne.js
+++ js/moduleOne.js
@@ -1,29 +1,56 @@
 
 
-
-angular.module('firstModule', [])
+angular.module('firstModule', [
+  'ngMessages'
+  ])
 
   .controller('FirstCtrl', ['$scope', function($scope) {
-    $scope.job_title = 'job title';
-    $scope.tedious_task = 'tedious task';
-    $scope.dirty_task = 'dirty task';
-    $scope.celebrity =  'celebrity';
-    $scope.useless_skill = 'useless skill';
-    $scope.obnoxious_celebrity = 'obnoxious celebrity';
-    $scope.huge_number = 'huge number';
-    $scope.adjective =  'adjective';
-    $scope.Name = 'Name';
-    $scope.gender = function(){
+    $scope.data = {
+      job_title : 'job title',
+      tedious_task : 'tedious task',
+      dirty_task : 'dirty task',
+      celebrity :  'celebrity',
+      useless_skill : 'useless skill',
+      obnoxious_celebrity : 'obnoxious celebrity',
+      number : 'huge number',
+      adjective :  'adjective',
+      Name : 'Name'
+    };
+
+    
+    $scope.gender = function() {
       if($scope.genderSingular === 'she') {
-        $scope.Name = 'Male name';
+        $scope.data.Name = 'Male name';
         $scope.genderSingular = 'he';
         $scope.genderObjective = 'him';
         $scope.genderPossessive = 'his';
       } else {
-        $scope.Name = 'Female name';
+        $scope.data.Name = 'Female name';
         $scope.genderSingular = 'she';
         $scope.genderObjective = 'her';
         $scope.genderPossessive = 'her';
       }
       };
+    $scope.submit = function() {
+        // if statements to check and identify individual form values
+
+      
+    $scope.checkValid = function(reset) {
+      if($scope.myForm.$submitted && $scope.myForm.$valid && !($scope.myForm.$pristine)) {
+        return true;
+      } else if (reset) {
+        return false; 
+      }
+    };
+      console.log($scope.myForm.$valid);
+      console.log($scope.myForm.$pristine);
+    };
+
+   $scope.resetForm = function() {
+      //hide textarea and show form
+      // I might need to add ng-click or function on the reset button to cause ng-show/hide
+        //to evaluate to true;
+      $scope.data = {};
+    };
+
 }]);
