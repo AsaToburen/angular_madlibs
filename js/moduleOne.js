a1f96ff036ba8384723c3b17438381a7f42e3c6b
Asa Toburen <atopettit@gmail.com>
2015-04-23 15:34:13 -0700

    styling

--- js/moduleOne.js
+++ js/moduleOne.js
@@ -1,48 +1,45 @@
+angular.module('madLibsMod', ['ngAnimate', 'ngMessages'])
 
+.controller('mainCtrl', ['$scope', function($scope) {
 
-angular.module('madLibsMod', ['ngAnimate', 'ngMessages'
-  ])
+  $scope.hide = false;
 
-  .controller('mainCtrl', ['$scope', function($scope) {
-    
+  $scope.data = {
+    gender: "Feminine",
+    genderSingular: "she",
+    genderObjective: "her",
+    genderPossessive: "her"
+  };
+
+  $scope.gender = function() {
+    if ($scope.data.genderSingular === "she") {
+      $scope.data.gender = "Masculine";
+      $scope.data.genderSingular = 'he';
+      $scope.data.genderObjective = 'him';
+      $scope.data.genderPossessive = 'his';
+    } else {
+      $scope.data.gender = "Feminine";
+      $scope.data.genderSingular = 'she';
+      $scope.data.genderObjective = 'her';
+      $scope.data.genderPossessive = 'her';
+    }
+  };
+
+  $scope.checkValid = function() {
+    if ($scope.myForm.$submitted && $scope.myForm.$valid && !($scope.myForm.$pristine)) {
+      $scope.hide = true;
+      return true;
+    }
+  };
+
+  $scope.resetForm = function() {
     $scope.hide = false;
-
     $scope.data = {
-      gender : "Female",
-      genderSingular : "she",
-      genderObjective : "her",
-      genderPossessive : "her"
+      gender: "Feminine",
+      genderSingular: "she",
+      genderObjective: "her",
+      genderPossessive: "her"
     };
-
-    $scope.gender = function() {
-      if($scope.data.genderSingular === "she") {
-        $scope.data.gender = "Male";
-        $scope.data.genderSingular = 'he';
-        $scope.data.genderObjective = 'him';
-        $scope.data.genderPossessive = 'his';
-      } else {
-        $scope.data.gender = "Female";
-        $scope.data.genderSingular = 'she';
-        $scope.data.genderObjective = 'her';
-        $scope.data.genderPossessive = 'her';
-      }
-      };
-      
-    $scope.checkValid = function() {
-      if($scope.myForm.$submitted && $scope.myForm.$valid && !($scope.myForm.$pristine)) {
-        $scope.hide = true;
-        return true;
-      }
-    };
-
-   $scope.resetForm = function() {
-    $scope.hide = false;
-      $scope.data = {
-        gender : "Female",
-        genderSingular : "she",
-        genderObjective : "her",
-        genderPossessive : "her"
-      };
-      $scope.myForm.$setPristine();
-    };
+    $scope.myForm.$setPristine();
+  };
 }]);
